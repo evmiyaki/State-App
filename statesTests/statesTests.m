a17065fa44527d3a45c9fd3bf7d4e940e67a3872
evmiyaki <emiyaki@CART115-LAP03.local>
2014-04-03 12:55:13 -1000

    first commit!
    nothing here yet!

---
+++ statesTests/statesTests.m
@@ -0,0 +1,34 @@
+//
+//  statesTests.m
+//  statesTests
+//
+//  Created by Evan Miyaki on 4/3/14.
+//  Copyright (c) 2014 chicagobulls. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface statesTests : XCTestCase
+
+@end
+
+@implementation statesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
